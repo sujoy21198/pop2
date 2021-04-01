85dd4a3cde229e0a546102f8a9bb264dd97d1f8b
ahazra7 <anirban.hazra@webstep.in>
2021-04-01 15:37:06 +0530

    Video click - on press next button, direct playing flag made false

--- Pop/Pages/StepSevenScreen.js
+++ Pop/Pages/StepSevenScreen.js
@@ -335,67 +335,55 @@ export default class StepSevenScreen extends Component {
     }
 
     setStepDataIntoPatch = async() => {
-        if(
-            (
-                this.state.cropSpecificSteps && this.state.cropSpecificSteps.length > 0 &&
-                this.state.cropSpecificSteps[6].videoFile
-            ) && !this.state.isPlaying
-        ) {
-            Toast.show({
-                type: 'warning',
-                duration: 3000,
-                text: 'Please pause the video before going to next screen'
-            })
-        } else {
-            try{
-                let username = await AsyncStorage.getItem('username')
-                let user = await AsyncStorage.getItem('cropData');
-                let parsed = JSON.parse(user);
-                var specificObject = parsed[0]
-                var cropSpecificSteps = specificObject.cropSteps.filter((i) => i.cropId === this.state._id)
-                if (this.state.saveButtonClicked === false) {
-                    alert("please click save button before procceding")
+        this.setState({isPlaying: false})
+        try{
+            let username = await AsyncStorage.getItem('username')
+            let user = await AsyncStorage.getItem('cropData');
+            let parsed = JSON.parse(user);
+            var specificObject = parsed[0]
+            var cropSpecificSteps = specificObject.cropSteps.filter((i) => i.cropId === this.state._id)
+            if (this.state.saveButtonClicked === false) {
+                alert("please click save button before procceding")
+            } else {
+                if (cropSpecificSteps[7] === undefined) {
+                    this.props.navigation.navigate({
+                        name: 'ActualCultivationCostScreen',
+                        params: {
+                            cropName: this.state.cropName,
+                            _id: this.state._id,
+                            imageFile: this.state.imageFile,
+                            patchName: this.state.patchName,
+                            landType: this.state.landType,
+                            farmingAreaInDecimal: this.state.farmingAreaInDecimal,
+                            costOfCultivatinPerTenDecimal: this.state.costOfCultivatinPerTenDecimal,
+                            costPerKg: this.state.costPerKg,
+                            productionInKg: this.state.productionInKg,
+                            cost: this.state.cost,
+                            netProfit: this.state.netProfit
+                        }
+                    })
                 } else {
-                    if (cropSpecificSteps[7] === undefined) {
-                        this.props.navigation.navigate({
-                            name: 'ActualCultivationCostScreen',
-                            params: {
-                                cropName: this.state.cropName,
-                                _id: this.state._id,
-                                imageFile: this.state.imageFile,
-                                patchName: this.state.patchName,
-                                landType: this.state.landType,
-                                farmingAreaInDecimal: this.state.farmingAreaInDecimal,
-                                costOfCultivatinPerTenDecimal: this.state.costOfCultivatinPerTenDecimal,
-                                costPerKg: this.state.costPerKg,
-                                productionInKg: this.state.productionInKg,
-                                cost: this.state.cost,
-                                netProfit: this.state.netProfit
-                            }
-                        })
-                    } else {
-                        this.props.navigation.navigate({
-                            name: 'StepEightScreen',
-                            params: {
-                                cropName: this.state.cropName,
-                                _id: this.state._id,
-                                imageFile: this.state.imageFile,
-                                patchName: this.state.patchName,
-                                landType: this.state.landType,
-                                farmingAreaInDecimal: this.state.farmingAreaInDecimal,
-                                costOfCultivatinPerTenDecimal: this.state.costOfCultivatinPerTenDecimal,
-                                costPerKg: this.state.costPerKg,
-                                productionInKg: this.state.productionInKg,
-                                cost: this.state.cost,
-                                netProfit: this.state.netProfit
-                            }
-                        })
-                    }
+                    this.props.navigation.navigate({
+                        name: 'StepEightScreen',
+                        params: {
+                            cropName: this.state.cropName,
+                            _id: this.state._id,
+                            imageFile: this.state.imageFile,
+                            patchName: this.state.patchName,
+                            landType: this.state.landType,
+                            farmingAreaInDecimal: this.state.farmingAreaInDecimal,
+                            costOfCultivatinPerTenDecimal: this.state.costOfCultivatinPerTenDecimal,
+                            costPerKg: this.state.costPerKg,
+                            productionInKg: this.state.productionInKg,
+                            cost: this.state.cost,
+                            netProfit: this.state.netProfit
+                        }
+                    })
                 }
-                AsyncStorage.setItem("jump","StepSevenScreen")
-            }catch(error){
-                console.log(error)
             }
+            AsyncStorage.setItem("jump","StepSevenScreen")
+        }catch(error){
+            console.log(error)
         }
     }
 
--- Pop/Pages/StepTwoScreen.js
+++ Pop/Pages/StepTwoScreen.js
@@ -332,69 +332,56 @@ export default class StepTwoScreen extends Component {
     }
 
     setStepDataIntoPatch = async () => {
-        if(
-            (
-                this.state.cropSpecificSteps && this.state.cropSpecificSteps.length > 0 &&
-                this.state.cropSpecificSteps[1].videoFile
-            ) &&
-            !this.state.isPlaying
-        ) {
-            Toast.show({
-                type: 'warning',
-                duration: 3000,
-                text: 'Please pause the video before going to next screen'
-            })
-        } else {
-            try {
-                let username = await AsyncStorage.getItem('username')
-                let user = await AsyncStorage.getItem('cropData');
-                let parsed = JSON.parse(user);
-                var specificObject = parsed[0]
-                var cropSpecificSteps = specificObject.cropSteps.filter((i) => i.cropId === this.state._id)
-                if (this.state.saveButtonClicked === false) {
-                    alert("please click save button before procceding")
+        this.setState({isPlaying: false})
+        try {
+            let username = await AsyncStorage.getItem('username')
+            let user = await AsyncStorage.getItem('cropData');
+            let parsed = JSON.parse(user);
+            var specificObject = parsed[0]
+            var cropSpecificSteps = specificObject.cropSteps.filter((i) => i.cropId === this.state._id)
+            if (this.state.saveButtonClicked === false) {
+                alert("please click save button before procceding")
+            } else {
+                if (cropSpecificSteps[2] === undefined) {
+                    this.props.navigation.navigate({
+                        name: 'ActualCultivationCostScreen',
+                        params: {
+                            cropName: this.state.cropName,
+                            _id: this.state._id,
+                            imageFile: this.state.imageFile,
+                            patchName: this.state.patchName,
+                            landType: this.state.landType,
+                            farmingAreaInDecimal: this.state.farmingAreaInDecimal,
+                            costOfCultivatinPerTenDecimal: this.state.costOfCultivatinPerTenDecimal,
+                            costPerKg: this.state.costPerKg,
+                            productionInKg: this.state.productionInKg,
+                            cost: this.state.cost,
+                            netProfit: this.state.netProfit
+                        }
+                    })
                 } else {
-                    if (cropSpecificSteps[2] === undefined) {
-                        this.props.navigation.navigate({
-                            name: 'ActualCultivationCostScreen',
-                            params: {
-                                cropName: this.state.cropName,
-                                _id: this.state._id,
-                                imageFile: this.state.imageFile,
-                                patchName: this.state.patchName,
-                                landType: this.state.landType,
-                                farmingAreaInDecimal: this.state.farmingAreaInDecimal,
-                                costOfCultivatinPerTenDecimal: this.state.costOfCultivatinPerTenDecimal,
-                                costPerKg: this.state.costPerKg,
-                                productionInKg: this.state.productionInKg,
-                                cost: this.state.cost,
-                                netProfit: this.state.netProfit
-                            }
-                        })
-                    } else {
-                        this.props.navigation.navigate({
-                            name: 'StepThreeScreen',
-                            params: {
-                                cropName: this.state.cropName,
-                                _id: this.state._id,
-                                imageFile: this.state.imageFile,
-                                patchName: this.state.patchName,
-                                landType: this.state.landType,
-                                farmingAreaInDecimal: this.state.farmingAreaInDecimal,
-                                costOfCultivatinPerTenDecimal: this.state.costOfCultivatinPerTenDecimal,
-                                costPerKg: this.state.costPerKg,
-                                productionInKg: this.state.productionInKg,
-                                cost: this.state.cost,
-                                netProfit: this.state.netProfit
-                            }
-                        })
-                    }
+                    this.props.navigation.navigate({
+                        name: 'StepThreeScreen',
+                        params: {
+                            cropName: this.state.cropName,
+                            _id: this.state._id,
+                            imageFile: this.state.imageFile,
+                            patchName: this.state.patchName,
+                            landType: this.state.landType,
+                            farmingAreaInDecimal: this.state.farmingAreaInDecimal,
+                            costOfCultivatinPerTenDecimal: this.state.costOfCultivatinPerTenDecimal,
+                            costPerKg: this.state.costPerKg,
+                            productionInKg: this.state.productionInKg,
+                            cost: this.state.cost,
+                            netProfit: this.state.netProfit
+                        }
+                    })
                 }
-                AsyncStorage.setItem("jump","StepTwoScreen")
-            } catch (error) {
-                console.log(error)
             }
-        }        
+            AsyncStorage.setItem("jump","StepTwoScreen")
+        } catch (error) {
+            console.log(error)
+        }       
 
     }
 
